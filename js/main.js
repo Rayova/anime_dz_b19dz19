bfc1548c1c84ad1b8a91f81f5bf24077a3e7ecb4
Rayova <55283119+Rayova@users.noreply.github.com>
2020-02-20 17:14:04 +0300

    animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-let box1animation = anime({
-  targets: '.delay-demo .el',
-  translateX: 250,
-  delay: 1000
-});
-let box1 = document.getElementByclass('box_1')
-box_1.onclick = box1animation.play
+// box_1
+let box_1animation = anime({
+  targets: '.box_1',
+  width: '100%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  autoplay: false
+})
+let b1 = document.getElementById('b1')
+b1.onclick = box_1animation.play
+
+// box_4
+let box_4animation = anime({
+  targets: '.box_4',
+  width: '70%', // -> from '28px' to '70%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  autoplay: false
+})
+let b4 = document.getElementById('b4')
+b4.onclick = box_4animation.play
+
+// box_5
+let box_5animation = anime({
+  targets: '.box_5',
+  height: '100%', // -> from '28px' to '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  autoplay: false
+})
+let b5 = document.getElementById('b5')
+b5.onclick = box_5animation.play
